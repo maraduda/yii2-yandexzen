e95501ec9faa07e118ac99757dba644e87aba1e7
Andrey Maraduda <a.maraduda@MacBook-Pro-Andrey.local>
2018-02-13 19:44:37 +0300

    use add

--- YandexZenView.php
+++ YandexZenView.php
@@ -2,6 +2,11 @@
 
 namespace Maraduda\yii\extensions\YandexZen;
 
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\helpers\ArrayHelper;
+use yii\widgets\BaseListView;
+use Zelenin\Feed;
 use Zelenin\Rss;
 
 class RssView extends Zelenin\Rss
